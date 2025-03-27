a36667189e97207ceda79f084a2af61cbc0aafd8
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-03-27 14:52:29 +0100

    feat: 3 new methods from the 3rd assignments

--- src/main/java/countries/Main.java
+++ src/main/java/countries/Main.java
@@ -1,7 +1,6 @@
 package countries;
-import java.util.Comparator;
-import java.util.List;
-import java.util.LongSummaryStatistics;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Main {
 
@@ -20,94 +19,123 @@ public class Main {
         //System.out.println(feladat16(countries));
         //feladat17(countries);
         //System.out.println(feladat20(countries));
-        System.out.println(feladat21(countries));
+        //System.out.println(feladat21(countries));
+        //----------------------------------------------------------------
+        //3.Feladatsor: redukciós műveletek
+        //System.out.println(feladat31(countries));
+        //feladat32(countries);
+        System.out.println(feladat33(countries));
     }
+    public static Country feladat31(List<Country> countries) {
+        return countries.stream()
+                .filter(country -> country.area() != null)
+                .max(Comparator.comparing(Country::area))
+                .get();
+    }
+
+    public static void feladat32(List<Country> countries) {
+        countries.stream()
+                .filter(country -> country.area() == null)
+                .forEach(System.out::println);
+    }
+    public static DoubleSummaryStatistics feladat33(List<Country> countries) {
+        return countries.stream()
+                .map(country -> country.area())
+                .filter(Objects::nonNull)
+                .mapToDouble(BigDecimal::doubleValue)
+                .summaryStatistics();
+    }
+
+
+    //---------------------------------------------------
     public static void feladat1(List<Country> countries) {
         countries.stream()
                 .map(country -> country.name())
                 .forEach(System.out::println);
     }
-    public static void feladat2(List<Country> countries){
+
+    public static void feladat2(List<Country> countries) {
         countries.stream()
                 .map(country -> country.capital())
                 .sorted(Comparator.nullsFirst(Comparator.naturalOrder()))
                 .forEach(System.out::println);
     }
-    public static long feladat4(List<Country> countries){
+
+    public static long feladat4(List<Country> countries) {
         return countries.stream()
                 .mapToLong(Country::population)
                 .max()
                 .getAsLong();
     }
 
-    public static double feladat5(List<Country> countries){
-        return  countries.stream()
+    public static double feladat5(List<Country> countries) {
+        return countries.stream()
                 .mapToLong(Country::population)
                 .average()
                 .getAsDouble();
     }
 
-    public static LongSummaryStatistics feladat6(List<Country> countries){
+    public static LongSummaryStatistics feladat6(List<Country> countries) {
         return countries.stream()
                 .mapToLong(Country::population)
                 .summaryStatistics();
     }
 
-    public static void feladat7(List<Country> countries){
+    public static void feladat7(List<Country> countries) {
         countries.stream().filter(country -> country.region() == Region.EUROPE)
                 .map(Country::name)
                 .forEach(System.out::println);
     }
 
-    public static long feladat8(List<Country> countries){
+    public static long feladat8(List<Country> countries) {
         return countries.stream().filter(country -> country.region() == Region.EUROPE)
                 .count();
     }
 
-    public static long feladat9(List<Country> countries){
+    public static long feladat9(List<Country> countries) {
         return countries.stream().filter(country -> country.independent()).count();
     }
 
-    public static void feladat10(List<Country> countries){
-        countries.stream().filter(country -> country.population()<100).map(Country::name).forEach(System.out::println);
+    public static void feladat10(List<Country> countries) {
+        countries.stream().filter(country -> country.population() < 100).map(Country::name).forEach(System.out::println);
     }
 
-    public static long feladat12(List<Country> countries){
+    public static long feladat12(List<Country> countries) {
         return countries.stream()
                 .filter(country -> country.region() == Region.EUROPE)
                 .mapToLong(Country::population).sum();
     }
 
-    public static String feladat16(List<Country> countries){
+    public static String feladat16(List<Country> countries) {
         return countries.stream()
                 .filter(country -> country.region() == Region.EUROPE)
                 .max(Comparator.comparingLong(Country::population))
                 .get().name();
     }
 
-    public static void feladat17(List<Country> countries){
+    public static void feladat17(List<Country> countries) {
         countries.stream()
                 .map(Country::name)
                 .limit(5).forEach(System.out::println);
     }
 
-    public static boolean feladat18(List<Country> countries){
+    public static boolean feladat18(List<Country> countries) {
         return countries.stream()
-                .anyMatch(country -> country.population()==0);
+                .anyMatch(country -> country.population() == 0);
     }
 
-    public static boolean feladat19(List<Country> countries){
+    public static boolean feladat19(List<Country> countries) {
         return countries.stream()
-                .allMatch(country -> country.timezones().size()>0);
+                .allMatch(country -> country.timezones().size() > 0);
     }
 
-    public static Country feladat20(List<Country> countries){
+    public static Country feladat20(List<Country> countries) {
         return countries.stream()
                 .filter(country -> country.name().startsWith("H"))
                 .findFirst().get();
     }
 
-    public static long feladat21(List<Country> countries){
+    public static long feladat21(List<Country> countries) {
         return countries.stream()
                 .flatMap(country -> country.timezones().stream()).distinct().count();
     }
